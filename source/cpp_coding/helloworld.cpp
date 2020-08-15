707c0b4ce443177299078c122794fab4f24cd272
adrinamin <adrin.amin@gmail.com>
2020-08-15 07:47:08 +0200

    update cpp_coding.

--- source/cpp_coding/helloworld.cpp
+++ source/cpp_coding/helloworld.cpp
@@ -15,9 +15,77 @@ T max2(T const& t1, T const& t2)
     return t1 < t2? t2: t1;
 }
 
+int DoubleIt(int const& x)
+{
+    return x*2;
+}
+
 
 int main()
 {
+    // references
+    int a = 3;
+    std::cout << "a is " << a << std::endl;
+    // initialize reference when you declare it
+    int& rA = a;
+    rA = 5;
+    std::cout << "a is " << a << std::endl;
+
+    Person Sally("Sally", "Doe", 234);
+    Person& rSally = Sally;
+    std::cout << "rSally is " << rSally.GetAge() << std::endl;
+
+    // pointers
+    // always initialize pointer.
+    // int* pA = nullptr -> null pointer are possible
+    int* pA = &a;
+    *pA = 4;
+    std::cout << "a is " << a << std::endl;
+    int b = 100;
+    pA = &b;
+    (*pA)++;
+    std::cout << "a " << a << ", *pA " << *pA << endl;
+
+    Person* pKate = &Sally;
+    std::cout << "*pKate: " << (*pKate).getName() << std::endl;
+    std::cout << "pKate->: " << pKate->getName() << std::endl;
+
+
+    // const
+    // commit to the compiler you won't change something
+    int i = 3;
+    int const ci = 3; // const after
+    i = 4;
+    // ci = 4 -> not possible
+
+    int&ri = i;
+    ri = 5;
+
+    int const & cri = i;
+    // cri = 6 -> not possible
+
+    // int& ncri = ci -> not possible
+
+    int j = 10;
+    int DoubleJ = DoubleIt(j);
+    int DoubleTen = DoubleIt(10);
+
+    Person blabla("blabla", "blubblub", 234);
+    Person const cbla = blabla;
+    int blaAge = cbla.GetAge();
+
+    // const with indirection
+    // refernces cannot retarget. so const means you can't change the value.
+
+    // pointers: you can declare the pointer to be const:
+    //int * const cpI
+    //Then you can't change it to point somewhere else
+    // cpI = &something -> not possible
+    // you can declare that the pointer poijnts at something const
+    // int const * cpI
+    // Then you can't use it to change the calue of the target
+    // *cpI = 2;
+
     std::cout << "max of 99 and 100 is " << max2(99,100) << std::endl;
     std::string firstname;
     std::string lastname;
